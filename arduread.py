65ea9f926c586c595662ca0e641e56ca4f7e88bc
Josh Cox <josh@webhosting.coop>
2015-03-29 21:48:58 -0500

    ardushipper initial import

---
+++ arduread.py
@@ -0,0 +1,33 @@
+import serial
+import syslog
+from time import sleep
+
+port = "/dev/ttyUSB1"
+
+ser = serial.Serial(port, 115200, timeout=15)
+syslog.openlog(logoption=syslog.LOG_PID, facility=syslog.LOG_DAEMON)
+ser.write("hello")
+
+count = 11
+while True:
+    count+= 1
+#    print count
+    if count > 10:
+      ser.write("1")
+      count = 1;
+    rawdata = ser.readline()
+    buff1 = "%s" % rawdata.split(b'\0',1)[0]
+    data = "%s" % buff1.strip()
+    if "BSQ26-ENDTRANSMISSION" in data:
+      break
+#    print len(data)
+    if not data.isspace():
+      if len(data) > 0:
+#        print 'Got:', data
+    	syslog.syslog(str(data))
+        ser.write("1")
+
+    sleep(0.5)
+#    print 'not blocked'
+
+ser.close()
---
+++ ardushipper.py
@@ -0,0 +1,21 @@
+import serial
+import syslog
+from time import sleep
+
+port = "/dev/ttyUSB1"
+
+ser = serial.Serial(port, 115200, timeout=15)
+syslog.openlog(logoption=syslog.LOG_PID, facility=syslog.LOG_DAEMON)
+
+while True:
+    rawdata = ser.readline()
+    buff1 = "%s" % rawdata.split(b'\0',1)[0]
+    data = "%s" % buff1.strip()
+    if "BSQ26-ENDTRANSMISSION" in data:
+      break
+    if not data.isspace():
+      if len(data) > 0:
+        print 'Got:', data
+        syslog.syslog(str(data))
+    sleep(0.5)
+ser.close()
