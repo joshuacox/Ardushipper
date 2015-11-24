004250e2ef05caaba7d0fed846017c271b82de5f
Josh Cox <josh@webhosting.coop>
2015-11-24 10:49:34 -0600

    test

--- ardushipper.py
+++ ardushipper.py
@@ -8,11 +8,16 @@ port = "/dev/ttyACM0"
 ser = serial.Serial(port, 115200, timeout=15)
 syslog.openlog(logoption=syslog.LOG_PID, facility=syslog.LOG_DAEMON)
 
+count = 0
+
 while True:
     rawdata = ser.readline()
     buff1 = "%s" % rawdata.split(b'\0',1)[0]
     data = "%s" % buff1.strip()
     if "3478-ENDTRANSMISSION" in data:
+      count++
+    if count > 2:
+      count = 0
       break
     if not data.isspace():
       if len(data) > 0:
